25b8e1d42625862cbbcf46d633f054a8de48144d
Carmelo Cascone <carmelo@opennetworking.org>
2018-06-19 20:32:14 +0200

    Added ONF copyright to test cases

--- tests/ptf/fabric.ptf/test.py
+++ tests/ptf/fabric.ptf/test.py
@@ -1,4 +1,5 @@
 # Copyright 2013-present Barefoot Networks, Inc.
+# Copyright 2018-present Open Networking Foundation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
