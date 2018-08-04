11e16d2836bd2bd6601d47fb6cf9c5e1a9625ac2
Carmelo Cascone <carmelo@opennetworking.org>
2018-08-03 17:27:14 -0700

    Enable checkstyle via Travis

--- tests/ptf/xnt.py
+++ tests/ptf/xnt.py
@@ -18,25 +18,28 @@
 from scapy.packet import *
 from scapy.fields import *
 
+
 class INT_META_HDR(Packet):
     name = "INT_metadata_header"
-    fields_desc = [ BitField("ver", 0, 2), BitField("rep", 0, 2),
-                    BitField("c", 0, 1), BitField("e", 0, 1),
-                    BitField("rsvd1", 0, 5), BitField("ins_cnt", 1, 5),
-                    BitField("max_hop_cnt", 32, 8),
-                    BitField("total_hop_cnt", 0, 8),
-                    ShortField("inst_mask", 0x8000),
-                    ShortField("rsvd2", 0x0000)]
+    fields_desc = [BitField("ver", 0, 2), BitField("rep", 0, 2),
+                   BitField("c", 0, 1), BitField("e", 0, 1),
+                   BitField("rsvd1", 0, 5), BitField("ins_cnt", 1, 5),
+                   BitField("max_hop_cnt", 32, 8),
+                   BitField("total_hop_cnt", 0, 8),
+                   ShortField("inst_mask", 0x8000),
+                   ShortField("rsvd2", 0x0000)]
+
 
 class INT_L45_HEAD(Packet):
     name = "INT_L45_HEAD"
-    fields_desc = [ XByteField("int_type", 0x01),
+    fields_desc = [XByteField("int_type", 0x01),
                    XByteField("rsvd0", 0x00),
                    XByteField("length", 0x00),
-                   XByteField("rsvd1", 0x00) ]
+                   XByteField("rsvd1", 0x00)]
+
 
 class INT_L45_TAIL(Packet):
     name = "INT_L45_TAIL"
-    fields_desc = [ XByteField("next_proto", 0x01),
+    fields_desc = [XByteField("next_proto", 0x01),
                    XShortField("proto_param", 0x0000),
-                   XByteField("rsvd", 0x00) ]
+                   XByteField("rsvd", 0x00)]
