02b09622427da523850da16db60312d402f55222
Carmelo Cascone <carmelo.cascone@gmail.com>
2018-09-20 15:00:27 -0700

    Test MPLS group forwarding in IPv4 routing tests (#39)
    Also test MPLS forwarding in SPGW and INT tests

--- tests/ptf/fabric.ptf/test.py
+++ tests/ptf/fabric.ptf/test.py
@@ -14,19 +14,17 @@
 # limitations under the License.
 #
 
-import unittest
 from itertools import combinations
 
 from ptf.testutils import group
-from scapy.contrib.mpls import MPLS
 
 from base_test import autocleanup
 from fabric_test import *
 
 vlan_confs = {
-    "tagged": [True, True],
-    "untagged": [False, False],
-    "mixed": [True, False],
+    "tag->tag": [True, True],
+    "untag->untag": [False, False],
+    "tag->untag": [True, False],
 }
 
 
@@ -228,34 +226,25 @@ class FabricIPv4MPLSTest(FabricTest):
         testutils.verify_packets(self, exp_pkt_1to2, [self.port2])
 
 
-class FabricIPv4MPLSGroupTest(FabricTest):
+class FabricIPv4MplsGroupTest(IPv4UnicastTest):
     @autocleanup
+    def doRunTest(self, pkt, mac_dest, tagged1):
+        self.runIPv4UnicastTest(
+            pkt, mac_dest, prefix_len=24, tagged1=tagged1, tagged2=False,
+            bidirectional=False, mpls=True)
+
     def runTest(self):
-        vlan_id = 10
-        self.set_ingress_port_vlan(self.port1, False, 0, vlan_id)
-        self.set_forwarding_type(self.port1, SWITCH_MAC, 0x800,
-                                 FORWARDING_TYPE_UNICAST_IPV4)
-        self.add_forwarding_routing_v4_entry(HOST2_IPV4, 24, 500)
-        grp_id = 77
-        mpls_label = 0xaba
-        mbrs = {2: (self.port2, SWITCH_MAC, HOST2_MAC, mpls_label)}
-        self.add_next_hop_mpls_v4_group(500, grp_id, mbrs)
-        self.set_egress_vlan_pop(self.port2, vlan_id)
-
-        pkt_1to2 = testutils.simple_tcp_packet(
-            eth_src=HOST1_MAC, eth_dst=SWITCH_MAC,
-            ip_src=HOST1_IPV4, ip_dst=HOST2_IPV4, ip_ttl=64)
-        exp_pkt_1to2 = testutils.simple_mpls_packet(
-            eth_src=SWITCH_MAC, eth_dst=HOST2_MAC,
-            mpls_tags=[{
-                "label": mpls_label,
-                "tc": 0,
-                "s": 1,
-                "ttl": DEFAULT_MPLS_TTL}],
-            inner_frame=pkt_1to2[IP:])
-
-        testutils.send_packet(self, self.port1, str(pkt_1to2))
-        testutils.verify_packets(self, exp_pkt_1to2, [self.port2])
+        print ""
+        for tagged1 in [True, False]:
+            for pkt_type in ["tcp", "udp", "icmp"]:
+                print "Testing %s packet with tagged=%s..." \
+                      % (pkt_type, tagged1)
+                pkt = getattr(testutils, "simple_%s_packet" % pkt_type)(
+                    eth_src=HOST1_MAC, eth_dst=SWITCH_MAC,
+                    ip_src=HOST1_IPV4, ip_dst=HOST2_IPV4,
+                    pktlen=MIN_PKT_LEN
+                )
+                self.doRunTest(pkt, HOST2_MAC, tagged1)
 
 
 @group("packetio")
@@ -329,121 +318,112 @@ class FabricDefaultVlanPacketInTest(FabricTest):
 @group("spgw")
 class SpgwDownlinkTest(SpgwSimpleTest):
     @autocleanup
+    def doRunTest(self, pkt, tagged1, tagged2, mpls):
+        self.runDownlinkTest(pkt=pkt, tagged1=tagged1,
+                             tagged2=tagged2, mpls=mpls)
+
     def runTest(self):
-        inner_udp = UDP(sport=5061, dport=5060) / ("\xab" * 128)
-        pkt = Ether(src=self.DMAC_2, dst=self.SWITCH_MAC_2) / \
-              IP(src=S1U_ENB_IPV4, dst=UE_IPV4) / \
-              inner_udp
-        exp_pkt = Ether(src=self.SWITCH_MAC_1, dst=self.DMAC_1) / \
-                  IP(tos=0, id=0x1513, flags=0, frag=0,
-                     src=S1U_SGW_IPV4, dst=S1U_ENB_IPV4) / \
-                  UDP(sport=UDP_GTP_PORT, dport=UDP_GTP_PORT, chksum=0) / \
-                  make_gtp(20 + len(inner_udp), 1) / \
-                  IP(src=S1U_ENB_IPV4, dst=UE_IPV4, ttl=63) / \
-                  inner_udp
-        testutils.send_packet(self, self.port2, str(pkt))
-        testutils.verify_packet(self, exp_pkt, self.port1)
+        print ""
+        for vlan_conf, tagged in vlan_confs.items():
+            for pkt_type in ["tcp", "udp", "icmp"]:
+                for mpls in [False, True]:
+                    if mpls and tagged[1]:
+                        continue
+                    print "Testing VLAN=%s, pkt=%s, mpls=%s..." \
+                          % (vlan_conf, pkt_type, mpls)
+                    pkt = getattr(testutils, "simple_%s_packet" % pkt_type)(
+                        eth_src=HOST1_MAC, eth_dst=SWITCH_MAC,
+                        ip_src=HOST1_IPV4, ip_dst=HOST2_IPV4,
+                        pktlen=MIN_PKT_LEN
+                    )
+                    self.doRunTest(pkt, tagged[0], tagged[1], mpls)
 
 
 @group("spgw")
 class SpgwUplinkTest(SpgwSimpleTest):
     @autocleanup
+    def doRunTest(self, pkt, tagged1, tagged2, mpls):
+        self.runUplinkTest(ue_out_pkt=pkt, tagged1=tagged1,
+                           tagged2=tagged2, mpls=mpls)
+
     def runTest(self):
-        inner_udp = UDP(sport=5060, dport=5061) / ("\xab" * 128)
-        pkt = Ether(src=self.DMAC_1, dst=self.SWITCH_MAC_1) / \
-              IP(src=S1U_ENB_IPV4, dst=S1U_SGW_IPV4) / \
-              UDP(sport=UDP_GTP_PORT, dport=UDP_GTP_PORT) / \
-              make_gtp(20 + len(inner_udp), 0xeeffc0f0) / \
-              IP(src=UE_IPV4, dst=S1U_ENB_IPV4) / \
-              inner_udp
-        exp_pkt = Ether(src=self.SWITCH_MAC_2, dst=self.DMAC_2) / \
-                  IP(src=UE_IPV4, dst=S1U_ENB_IPV4, ttl=63) / \
-                  inner_udp
-        testutils.send_packet(self, self.port1, str(pkt))
-        testutils.verify_packet(self, exp_pkt, self.port2)
+        print ""
+        for vlan_conf, tagged in vlan_confs.items():
+            for pkt_type in ["tcp", "udp", "icmp"]:
+                for mpls in [False, True]:
+                    if mpls and tagged[1]:
+                        continue
+                    print "Testing VLAN=%s, pkt=%s, mpls=%s..." \
+                          % (vlan_conf, pkt_type, mpls)
+                    pkt = getattr(testutils, "simple_%s_packet" % pkt_type)(
+                        eth_src=HOST1_MAC, eth_dst=SWITCH_MAC,
+                        ip_src=HOST1_IPV4, ip_dst=HOST2_IPV4,
+                        pktlen=MIN_PKT_LEN
+                    )
+                    self.doRunTest(pkt, tagged[0], tagged[1], mpls)
 
 
-@group("spgw")
-class SpgwDownlinkMPLSTest(SpgwMPLSTest):
-    @autocleanup
-    def runTest(self):
-        inner_udp = UDP(sport=5061, dport=5060) / ("\xab" * 128)
-        pkt = Ether(src=self.DMAC_2, dst=self.SWITCH_MAC_2) / \
-              IP(src=S1U_ENB_IPV4, dst=UE_IPV4) / \
-              inner_udp
-        exp_pkt = Ether(src=self.SWITCH_MAC_1, dst=self.DMAC_1) / \
-                  MPLS(label=self.mpls_label, cos=0, s=1, ttl=64) / \
-                  IP(tos=0, id=0x1513, flags=0, frag=0,
-                     src=S1U_SGW_IPV4, dst=S1U_ENB_IPV4) / \
-                  UDP(sport=UDP_GTP_PORT, dport=UDP_GTP_PORT, chksum=0) / \
-                  make_gtp(20 + len(inner_udp), 1) / \
-                  IP(src=S1U_ENB_IPV4, dst=UE_IPV4, ttl=64) / \
-                  inner_udp
-        testutils.send_packet(self, self.port2, str(pkt))
-        testutils.verify_packet(self, exp_pkt, self.port1)
-
-
-@group("spgw")
-@unittest.skip("INT transit capability not yet supported")
-class SpgwDownlinkMPLS_INT_Test(SpgwMPLSTest):
-    @autocleanup
-    def runTest(self):
-        self.setup_int()
-
-        dport = 5060
-
-        # int_type=hop-by-hop
-        int_shim = INT_L45_HEAD(int_type=1, length=4)
-        # ins_cnt: 5 = switch id + ports + q occupancy + ig port + eg port)
-        # max_hop_count: 3
-        # total_hop_count: 0
-        # instruction_mask_0003: 0xd = switch id (0), ports (1), q occupancy (3)
-        # instruction_mask_0407: 0xc = ig timestamp (4), eg timestamp (5)
-        int_header = "\x00\x05\x03\x00\xdc\x00\x00\x00"
-        # IP proto (UDP), UDP dport (4096)
-        int_tail = INT_L45_TAIL(next_proto=17, proto_param=dport)
-
-        payload = "\xab" * 128
-        inner_udp = UDP(sport=5061, dport=dport, chksum=0)
-        # IP tos is 0x04 to enable INT
-        pkt = Ether(src=self.DMAC_2, dst=self.SWITCH_MAC_2) / \
-              IP(tos=0x04, src=S1U_ENB_IPV4, dst=UE_IPV4) / \
-              inner_udp / \
-              int_shim / int_header / int_tail / \
-              payload
-
-        exp_int_shim = INT_L45_HEAD(int_type=1, length=9)
-        # total_hop_count: 1
-        exp_int_header = "\x00\x05\x03\x01\xdc\x00\x00\x00"
-        # switch id: 1
-        exp_int_metadata = "\x00\x00\x00\x01"
-        # ig port: port2, eg port: port2
-        exp_int_metadata += stringify(self.port2, 2) + stringify(self.port1, 2)
-        # q id: 0, q occupancy: ?
-        exp_int_metadata += "\x00\x00\x00\x00"
-        # ig timestamp: ?
-        # eg timestamp: ?
-        exp_int_metadata += "\x00\x00\x00\x00" * 2
-
-        exp_int = exp_int_shim / exp_int_header / exp_int_metadata / int_tail
-
-        exp_pkt = Ether(src=self.SWITCH_MAC_1, dst=self.DMAC_1) / \
-                  MPLS(label=self.mpls_label, cos=0, s=1, ttl=64) / \
-                  IP(tos=0, id=0x1513, flags=0, frag=0,
-                     src=S1U_SGW_IPV4, dst=S1U_ENB_IPV4) / \
-                  UDP(sport=UDP_GTP_PORT, dport=UDP_GTP_PORT, chksum=0) / \
-                  make_gtp(20 + len(inner_udp) + len(exp_int) + len(payload), 1) / \
-                  IP(tos=0x04, src=S1U_ENB_IPV4, dst=UE_IPV4, ttl=64) / \
-                  inner_udp / \
-                  exp_int / \
-                  payload
-        # We mask off the timestamps as well as the queue occupancy
-        exp_pkt = Mask(exp_pkt)
-        offset_metadata = 14 + 4 + 20 + 8 + 8 + 20 + 8 + 4 + 8
-        exp_pkt.set_do_not_care((offset_metadata + 9) * 8, 11 * 8)
-
-        testutils.send_packet(self, self.port2, str(pkt))
-        testutils.verify_packet(self, exp_pkt, self.port1)
+# @group("spgw")
+# @unittest.skip("INT transit capability not yet supported")
+# class SpgwDownlinkMPLS_INT_Test(SpgwMPLSTest):
+#     @autocleanup
+#     def runTest(self):
+#         self.setup_int()
+#
+#         dport = 5060
+#
+#         # int_type=hop-by-hop
+#         int_shim = INT_L45_HEAD(int_type=1, length=4)
+#         # ins_cnt: 5 = switch id + ports + q occupancy + ig port + eg port)
+#         # max_hop_count: 3
+#         # total_hop_count: 0
+#         # instruction_mask_0003: 0xd = switch id (0), ports (1), q occupancy (3)
+#         # instruction_mask_0407: 0xc = ig timestamp (4), eg timestamp (5)
+#         int_header = "\x00\x05\x03\x00\xdc\x00\x00\x00"
+#         # IP proto (UDP), UDP dport (4096)
+#         int_tail = INT_L45_TAIL(next_proto=17, proto_param=dport)
+#
+#         payload = "\xab" * 128
+#         inner_udp = UDP(sport=5061, dport=dport, chksum=0)
+#         # IP tos is 0x04 to enable INT
+#         pkt = Ether(src=self.DMAC_2, dst=self.SWITCH_MAC_2) / \
+#               IP(tos=0x04, src=S1U_ENB_IPV4, dst=UE_IPV4) / \
+#               inner_udp / \
+#               int_shim / int_header / int_tail / \
+#               payload
+#
+#         exp_int_shim = INT_L45_HEAD(int_type=1, length=9)
+#         # total_hop_count: 1
+#         exp_int_header = "\x00\x05\x03\x01\xdc\x00\x00\x00"
+#         # switch id: 1
+#         exp_int_metadata = "\x00\x00\x00\x01"
+#         # ig port: port2, eg port: port2
+#         exp_int_metadata += stringify(self.port2, 2) + stringify(self.port1, 2)
+#         # q id: 0, q occupancy: ?
+#         exp_int_metadata += "\x00\x00\x00\x00"
+#         # ig timestamp: ?
+#         # eg timestamp: ?
+#         exp_int_metadata += "\x00\x00\x00\x00" * 2
+#
+#         exp_int = exp_int_shim / exp_int_header / exp_int_metadata / int_tail
+#
+#         exp_pkt = Ether(src=self.SWITCH_MAC_1, dst=self.DMAC_1) / \
+#                   MPLS(label=self.mpls_label, cos=0, s=1, ttl=64) / \
+#                   IP(tos=0, id=0x1513, flags=0, frag=0,
+#                      src=S1U_SGW_IPV4, dst=S1U_ENB_IPV4) / \
+#                   UDP(sport=UDP_GTP_PORT, dport=UDP_GTP_PORT, chksum=0) / \
+#                   make_gtp(20 + len(inner_udp) + len(exp_int) + len(payload), 1) / \
+#                   IP(tos=0x04, src=S1U_ENB_IPV4, dst=UE_IPV4, ttl=64) / \
+#                   inner_udp / \
+#                   exp_int / \
+#                   payload
+#         # We mask off the timestamps as well as the queue occupancy
+#         exp_pkt = Mask(exp_pkt)
+#         offset_metadata = 14 + 4 + 20 + 8 + 8 + 20 + 8 + 4 + 8
+#         exp_pkt.set_do_not_care((offset_metadata + 9) * 8, 11 * 8)
+#
+#         testutils.send_packet(self, self.port2, str(pkt))
+#         testutils.verify_packet(self, exp_pkt, self.port1)
 
 
 @group("int")
@@ -473,8 +453,14 @@ class FabricIntSourceTest(IntTest):
 @group("int")
 class FabricIntSourceAndTransitTest(IntTest):
     @autocleanup
-    def doRunTest(self, **kwargs):
-        self.runIntSourceTest(**kwargs)
+    def doRunTest(self, vlan_conf, tagged, pkt_type, mpls, instrs):
+        print "Testing VLAN=%s, pkt=%s, mpls=%s, instructions=%s..." \
+              % (vlan_conf, pkt_type, mpls,
+                 ",".join([INT_INS_TO_NAME[i] for i in instrs]))
+        pkt = getattr(testutils, "simple_%s_packet" % pkt_type)()
+        self.runIntSourceTest(pkt=pkt, instructions=instrs,
+                              with_transit=True, ignore_csum=True,
+                              tagged1=tagged[0], tagged2=tagged[1], mpls=mpls)
 
     def runTest(self):
         instr_sets = [
@@ -485,21 +471,30 @@ class FabricIntSourceAndTransitTest(IntTest):
         print ""
         for vlan_conf, tagged in vlan_confs.items():
             for pkt_type in ["udp", "tcp"]:
-                for instrs in instr_sets:
-                    print "Testing VLAN=%s, pkt=%s, instructions=%s..." \
-                          % (vlan_conf, pkt_type,
-                             ",".join([INT_INS_TO_NAME[i] for i in instrs]))
-                    pkt = getattr(testutils, "simple_%s_packet" % pkt_type)()
-                    self.doRunTest(pkt=pkt, instructions=instrs,
-                                   with_transit=True, ignore_csum=True,
-                                   tagged1=tagged[0], tagged2=tagged[1])
+                for mpls in [False, True]:
+                    for instrs in instr_sets:
+                        if mpls and tagged[1]:
+                            continue
+                        self.doRunTest(vlan_conf, tagged, pkt_type, mpls,
+                                       instrs)
 
 
 @group("int")
 class FabricIntTransitTest(IntTest):
     @autocleanup
-    def doRunTest(self, **kwargs):
-        self.runIntTransitTest(**kwargs)
+    def doRunTest(self, vlan_conf, tagged, pkt_type, prev_hops, instrs, mpls):
+        print "Testing VLAN=%s, pkt=%s, mpls=%s, prev_hops=%s, instructions=%s..." \
+              % (vlan_conf, pkt_type, mpls, prev_hops,
+                 ",".join([INT_INS_TO_NAME[i] for i in instrs]))
+        pkt = getattr(testutils, "simple_%s_packet" % pkt_type)()
+        hop_metadata, _ = self.get_int_metadata(instrs, 0xCAFEBABE, 0xDEAD, 0xBEEF)
+        int_pkt = self.get_int_pkt(pkt=pkt, instructions=instrs, max_hop=50,
+                                   transit_hops=prev_hops,
+                                   hop_metadata=hop_metadata)
+        self.runIntTransitTest(pkt=int_pkt,
+                               tagged1=tagged[0],
+                               tagged2=tagged[1],
+                               ignore_csum=1, mpls=mpls)
 
     def runTest(self):
         instr_sets = [
@@ -509,20 +504,13 @@ class FabricIntTransitTest(IntTest):
         print ""
         for vlan_conf, tagged in vlan_confs.items():
             for pkt_type in ["udp", "tcp"]:
-                for prev_hops in [0, 3]:
-                    for instructions in instr_sets:
-                        print "Testing VLAN=%s, pkt=%s, prev_hops=%s, instructions=%s..." \
-                              % (vlan_conf, pkt_type, prev_hops,
-                                 ",".join([INT_INS_TO_NAME[i] for i in instructions]))
-                        pkt = getattr(testutils, "simple_%s_packet" % pkt_type)()
-                        hop_metadata, _ = self.get_int_metadata(
-                            instructions, 0xCAFEBABE, 0xDEAD, 0xBEEF)
-                        int_pkt = self.get_int_pkt(
-                            pkt=pkt, instructions=instructions, max_hop=50,
-                            transit_hops=prev_hops, hop_metadata=hop_metadata)
-                        self.doRunTest(
-                            pkt=int_pkt, tagged1=tagged[0], tagged2=tagged[1],
-                            ignore_csum=1)
+                for mpls in [False, True]:
+                    for prev_hops in [0, 3]:
+                        for instrs in instr_sets:
+                            if mpls and tagged[1]:
+                                continue
+                            self.doRunTest(vlan_conf, tagged, pkt_type,
+                                           prev_hops, instrs, mpls)
 
 
 @group("int")
