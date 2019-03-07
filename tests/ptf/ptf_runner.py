aa05a0f43aa8dcc97f4339a0ed1410acc03c900d
Remi Pelland <remi.pelland@noviflow.com>
2019-03-07 11:17:57 -0500

    Addition of --skip-launch parameter to allow configuration and test execution without automatically starting the switch.  Typically used with bmv2 and stratum-bmv2 devices when manual launch is desired.

--- tests/ptf/ptf_runner.py
+++ tests/ptf/ptf_runner.py
@@ -286,6 +286,9 @@ def main():
     parser.add_argument('--skip-test',
                         help='Skip test execution (useful to perform only pipeline configuration)',
                         action="store_true", default=False)
+    parser.add_argument('--skip-launch',
+                        help='Skip switch launch (requires that switch be started manually beforehand)',
+                        action="store_true", default=False)
     args, unknown_args = parser.parse_known_args()
 
     if not check_ptf():
@@ -322,21 +325,22 @@ def main():
     grpc_port = args.grpc_addr.split(':')[1]
 
     bmv2_sw = None
-    if device == 'bmv2':
-        bmv2_sw = Bmv2Switch(device_id=args.device_id,
-                             port_map_path=args.port_map,
-                             grpc_port=grpc_port,
-                             cpu_port=args.cpu_port,
-                             loglevel='debug')
-        bmv2_sw.start()
-    elif device == 'stratum-bmv2':
-        bmv2_sw = Bmv2Switch(device_id=args.device_id,
-                             port_map_path=args.port_map,
-                             grpc_port=grpc_port,
-                             cpu_port=args.cpu_port,
-                             loglevel='debug',
-                             is_stratum=True)
-        bmv2_sw.start()
+    if args.skip_launch == False:
+        if device == 'bmv2':
+            bmv2_sw = Bmv2Switch(device_id=args.device_id,
+                                 port_map_path=args.port_map,
+                                 grpc_port=grpc_port,
+                                 cpu_port=args.cpu_port,
+                                 loglevel='debug')
+            bmv2_sw.start()
+        elif device == 'stratum-bmv2':
+            bmv2_sw = Bmv2Switch(device_id=args.device_id,
+                                 port_map_path=args.port_map,
+                                 grpc_port=grpc_port,
+                                 cpu_port=args.cpu_port,
+                                 loglevel='debug',
+                                 is_stratum=True)
+            bmv2_sw.start()
 
     try:
 
