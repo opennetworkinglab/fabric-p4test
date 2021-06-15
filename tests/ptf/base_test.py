dbcf2122f7b9d82b767b9a48d1f126d909ab9687
Wailok Shum <wailok@opennetworking.org>
2021-06-16 05:13:19 +0800

    revert verify_no_other_packets

--- tests/ptf/base_test.py
+++ tests/ptf/base_test.py
@@ -355,9 +355,9 @@ class P4RuntimeTest(BaseTest):
             pkt, port=out_port, do_forwarding=False))
         self.verify_packet(pkt, out_port)
 
-    def verify_no_other_packets(self, timeout=2):
+    def verify_no_other_packets(self):
         if not self.generate_tv:
-            testutils.verify_no_other_packets(self, timeout=timeout)
+            testutils.verify_no_other_packets(self)
 
     def get_stream_packet(self, type_, timeout=1):
         start = time.time()
